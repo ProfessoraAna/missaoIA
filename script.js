6c7beaf8bc7ec7d0620c0e5d1547100d8a6e3e54
ANA CARLA <100882665+ProfessoraAna@users.noreply.github.com>
2025-08-11 11:44:41 -0300

    aula 2

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-const caixaPrincipal = document.querySelector(".caixa-principal") 
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativa  = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado: "Pergunta 1",
+    Alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+
+{
+    enunciado: "Pergunta 2",
+    Alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+
+{
+    enunciado: "Pergunta 3",
+    Alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+
+{
+    enunciado: "Pergunta 4",
+    Alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+
+{
+    enunciado: "Pergunta 5",
+    Alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+
+];
